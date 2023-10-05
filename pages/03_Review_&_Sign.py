257b825bca704ad224ffffc69f574898e33ab45b
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2023-10-05 12:56:48 +0200

    Update 03_Review_&_Sign.py

--- pages/03_Review_&_Sign.py
+++ pages/03_Review_&_Sign.py
@@ -5,7 +5,7 @@ from dropbox_sign import \
     ApiClient, ApiException, Configuration, apis, models
 
 
-st.set_page_config(page_title="ProSign - AI Powered Contract Review & Signing", page_icon="📝", layout="wide",
+st.set_page_config(page_title="ProSign - AI Powered NDA Review & Signing", page_icon="📝", layout="wide",
                    initial_sidebar_state="auto", menu_items=None)
 
 page_bg_img = f"""
@@ -35,34 +35,34 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 if 'file_name' not in st.session_state.keys():
     st.session_state['file_name'] = None
 
-st.title("ProSign - AI Powered Contract Review & Signing 📝")
+st.title("ProSign - AI Powered NDA Review & Signing 📝")
+
+configuration = Configuration(
+            username=st.secrets["dropbox_credentials"]["username"])
 
 if st.session_state['file_name'] is not None:
     st.header('Review and Sign {}'.format(st.session_state['file_name']))
     contract_name = st.session_state['file_name'].split('.')[0]
 
     with st.sidebar:
-        name = st.text_input('Your Full Name')
-        email = st.text_input('Your Email Address')
+        name = st.text_input(' ', placeholder='Your Full Name')
+        email = st.text_input(' ', placeholder='Your Email Address')
         click = st.button('Review and Sign')
 
-    if click and not name or not email:
+    if click and (not name or not email):
         st.error('Please enter your name and email address.')
-    
+
     elif click and name and email:
-        configuration = Configuration(
-            # Configure HTTP basic authorization: api_key
-            username=st.secrets["dropbox_credentials"]["username"])
 
         with ApiClient(configuration) as api_client:
             signature_request_api = apis.SignatureRequestApi(api_client)
-
+        
             signer_1 = models.SubSignatureRequestSigner(
                 email_address=email,
                 name=name,
                 order=0,
             )
-
+        
             signing_options = models.SubSigningOptions(
                 draw=True,
                 type=True,
@@ -70,7 +70,7 @@ if st.session_state['file_name'] is not None:
                 phone=True,
                 default_type="draw",
             )
-
+        
             data = models.SignatureRequestCreateEmbeddedRequest(
                 client_id=st.secrets["dropbox_credentials"]["client_id"],
                 title=contract_name,
@@ -80,22 +80,24 @@ if st.session_state['file_name'] is not None:
                 test_mode=True,
             )
 
-            try:
-                response = signature_request_api.signature_request_create_embedded(data)
-                signature_id = response['signature_request']['signatures'][0]['signature_id']
-                embedded_api = apis.EmbeddedApi(api_client)
+            response = signature_request_api.signature_request_create_embedded(data)
+            signature_id = response['signature_request']['signatures'][0]['signature_id']
+            signature_request_id = response['signature_request']['signature_request_id']
 
-                signature_id = signature_id
-                response = embedded_api.embedded_sign_url(signature_id)
+            embedded_api = apis.EmbeddedApi(api_client)
 
-                sign_url = response['embedded']['sign_url']
-                url = sign_url + st.secrets["dropbox_credentials"]["embedded_url"]
+            response = embedded_api.embedded_sign_url(signature_id)
 
-                # display the sign url
-                components.iframe(url, width=1500, height=1000, scrolling=True)
+            sign_url = response['embedded']['sign_url']
+            url = sign_url + st.secrets["dropbox_credentials"]["embedded_url"]
+            
+            # display the sign url
+            components.iframe(url, width=1500, height=1000, scrolling=True)
 
-            except ApiException as e:
-                st.error("Exception when calling Dropbox Sign API: %s\n" % e)
+            response = signature_request_api.signature_request_files_as_file_url(signature_request_id)
+
+            with st.sidebar:
+                st.link_button(label='Download Signed Document', url=response['file_url'])
 
 
 footer_html = """
@@ -116,7 +118,7 @@ footer_html = """
             text-decoration: none;
         }
     </style>
-        Made for Dropbox Sign AI Hackathon 2023. Powered by LlamaIndex 🦙
+        Made for Dropbox Sign AI Hackathon 2023. Powered by LlamaIndex and OpenAI.
     </div>
 """
 st.markdown(footer_html, unsafe_allow_html=True)
