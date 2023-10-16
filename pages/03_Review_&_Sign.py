2ccb286df8630e318a9f6f0c053712005b46676f
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2023-10-16 14:50:49 +0200

    Update 03_Review_&_Sign.py

--- pages/03_Review_&_Sign.py
+++ pages/03_Review_&_Sign.py
@@ -5,8 +5,7 @@ from dropbox_sign import \
     ApiClient, ApiException, Configuration, apis, models
 import time
 
-st.set_page_config(page_title="ProSign - AI Powered NDA Review & Signing", page_icon="📝", layout="wide",
-                   initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="ProSign - AI Powered NDA Review & Signing", page_icon="📝", layout="wide", menu_items=None)
 
 # def download():
 #     configuration = Configuration(
